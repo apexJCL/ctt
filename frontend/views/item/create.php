0f998ed915acf6777b89afbd363698d3bbcfddd4
José Carlos López <12030800@itcelaya.edu.mx>
2016-10-16 14:51:01 -0500

    test sh

--- frontend/views/item/create.php
+++ frontend/views/item/create.php
@@ -14,15 +14,13 @@ $this->title = Yii::t('app', 'Create Item');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Items'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div>
-    <?= $this->render('//layouts/_section_header') ?>
-    <div class="section greedy grey lighten-4">
-        <div class="container">
-            <?= $this->render('_form', [
-                'model' => $model,
-                'categories' => $categories,
-                'brands' => $brands
-            ]) ?>
-        </div>
+<?= $this->render('//layouts/_section_header') ?>
+<div class="container-fluid grey lighten-4">
+    <div class="container greedy">
+        <?= $this->render('_form', [
+            'model' => $model,
+            'categories' => $categories,
+            'brands' => $brands
+        ]) ?>
     </div>
 </div>
---
+++ post-receive.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Updating Composer Dependencies..."
+composer update -d $GIT_WORK_TREE
