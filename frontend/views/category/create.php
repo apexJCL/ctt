c07796566f4816cea9a05a01e81b54152c84ba52
José Carlos López <12030800@itcelaya.edu.mx>
2016-10-16 14:34:31 -0500

    debug line test

--- frontend/views/category/create.php
+++ frontend/views/category/create.php
@@ -1,23 +1,16 @@
 <?php
 
-use yii\helpers\Html;
-
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Category */
 
 $this->title = Yii::t('app', 'Create Category');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Categories'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div>
-    <?= $this->render('//layouts/_section_header') ?>
-
-    <div class="section grey lighten-4 greedy">
-        <div class="container-lazy">
-            <?= $this->render('_form', [
-                'model' => $model,
-            ]) ?>
-        </div>
+<?= $this->render('//layouts/_section_header') ?>
+<div class="container-fluid grey lighten-4">
+    <div class="container greedy">
+        <?= $this->render('_form', [
+            'model' => $model,
+        ]) ?>
     </div>
 </div>
